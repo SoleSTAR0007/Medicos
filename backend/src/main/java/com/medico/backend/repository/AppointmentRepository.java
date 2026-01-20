e6f762d76bfb824f83569c7991af3dfc67983c24
SoleSTAR0007 <bivaspaul.self7@gmail.com>
2026-01-20 05:40:35 +0530

    Implement appointment booking flow with doctor availability and time slots

--- backend/src/main/java/com/medico/backend/repository/AppointmentRepository.java
+++ backend/src/main/java/com/medico/backend/repository/AppointmentRepository.java
@@ -3,5 +3,11 @@ package com.medico.backend.repository;
 import com.medico.backend.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+   List<Appointment> findByPatientId(Long patientId);
+
+   List<Appointment> findByDoctorId(Long doctorId);
 }
--- backend/src/main/java/com/medico/backend/service/AppointmentService.java
+++ backend/src/main/java/com/medico/backend/service/AppointmentService.java
@@ -1,29 +1,33 @@
 package com.medico.backend.service;
 
 import com.medico.backend.model.Appointment;
-import org.springframework.stereotype.Service;
+import com.medico.backend.repository.AppointmentRepository;
 
-import java.util.ArrayList;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
 public class AppointmentService {
 
-    private final List<Appointment> appointments = new ArrayList<>();
+    private final AppointmentRepository appointmentRepository;
 
-    public Appointment bookAppointment(Long patientId,Long doctorId, String problem, String date,String time)
-    {
+    public AppointmentService(AppointmentRepository appointmentRepository) {this.appointmentRepository = appointmentRepository;
+    }
+
+    public Appointment bookAppointment(Long patientId,
+                                        Long doctorId,
+                                        String problem,
+                                        String date,
+                                        String time) {
 
         Appointment appointment =
                 new Appointment(patientId, doctorId, problem, date, time);
 
-        appointments.add(appointment);
-        return appointment;
+        return appointmentRepository.save(appointment);
     }
 
     public List<Appointment> getAppointmentsByPatient(Long patientId) {
-        return appointments.stream()
-                .filter(a -> a.getPatientId().equals(patientId))
-                .toList();
+        return appointmentRepository.findByPatientId(patientId);
     }
 }
+
